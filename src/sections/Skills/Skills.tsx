7a78a0a5b157fa59021f5ac4bebbc359f6462624
wilbernp <wilbernarvaezpetro@gmail.com>
2023-02-18 22:54:42 -0500

    add responsive to skills

--- src/sections/Skills/Skills.tsx
+++ src/sections/Skills/Skills.tsx
@@ -4,15 +4,21 @@ import SkillsRender from './SkillsRender'
 
 export default function Skills() {
   return (
-    <div>
-      <h2 className='text-center text-blue-500 text-4xl font-bold'>HABILIDADES</h2>
-       <h3 className='text-center text-blue-500 text-3xl font-medium'>Tecnologias</h3> 
+    <div className='mt-24'>
+      {/* ilustracion */}
+       <div className='w-[60%] h-[50vh] bg-slate-400 mx-auto mb-14'></div>
+       {/* titulo */}
+      <h2 className='text-center text-blue-500 text-3xl font-bold sm:text-4xl'>HABILIDADES</h2>
+      {/* subtitulo 1 */}
+       <h3 className='text-center text-white text-3xl font-medium my-6'>Tecnologias</h3> 
+       {/* tecnologias */}
        <SkillsRender 
        skills={technologiesData}
        className="max-w-[35rem]"
        />
-
-       <h3 className='text-center text-blue-500 text-3xl font-medium'>Habilidades Blandas</h3> 
+       {/* subtitulo 2 */}
+       <h3 className='text-center text-white text-3xl font-medium mt-14 mb-6'>Habilidades Blandas</h3> 
+       {/* habilidades blandas */}
        <SkillsRender 
        skills={softSkillsData}
        className="max-w-[33rem]"
