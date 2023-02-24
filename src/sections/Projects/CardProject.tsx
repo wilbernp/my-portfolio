42f4267e9170a8e3fd735ec059c7b43698cccd54
wilbernp <wilbernarvaezpetro@gmail.com>
2023-02-24 14:30:16 -0500

    add animation in project card

--- src/sections/Projects/CardProject.tsx
+++ src/sections/Projects/CardProject.tsx
@@ -8,23 +8,37 @@ import livePreview from '@iconify/icons-pajamas/live-preview';
 export default function CardProject(props: IProject) {
     return (
         <div
-            className='text-white bg-slate-600 w-80 py-7 px-5 text-center'
+            className='group/card overflow-hidden text-white bg-[#0d3263] w-80 py-7 text-center rounded-lg'
         >
             {/* titulo del proyecto */}
             <h2 className='text-2xl font-semibold'>{props.title}</h2>
-            {/* descripcion */}
-            <p className='my-5'>{props.description}</p>
-            {/* tecnologias */}
-            <span className='text-sm'>{props.tecnoligies}</span>
+            {/* container */}
+            <div className='relative'>
+                {/* imagen */}
+                <div className='w-full h-60 bg-slate-500 transition-[background-color] duration-300 ease-in-out group-hover/card:bg-[#0d3263]'></div>
+
+                {/* description */}
+                <p className='px-3 py-6 rounded-tr-2xl rounded-br-2xl text-start bg-[#072146] w-[90%] -translate-x-full absolute top-0 left-0 transition-transform ease-out duration-[900ms] group-hover/card:translate-x-0'>{props.description}</p>
+
+                {/* tecnologias */}
+                <p className='text-sm px-3 py-6 rounded-tl-2xl rounded-bl-2xl text-start bg-[#072146] w-[90%] translate-x-full absolute bottom-0 right-0 transition-transform ease-out duration-[900ms] group-hover/card:-translate-x-0'>{props.tecnoligies}</p>
+            </div>
+
             {/* links */}
-            <div className='flex justify-center items-center gap-2 mt-4'>
+            <div className='flex justify-center items-center gap-3 mt-4'>
                 {/* github link */}
-                <a href={props.github}>
+                <a 
+                href={props.github}
+                className="hover:scale-125 transition-transform duration-200 ease-in-out"
+                >
                     <InlineIcon className='text-[2.1rem]' icon={githubIcon} />
                 </a>
 
                 {/* deploy link */}
-                <a href={props.deploy}>
+                <a 
+                href={props.deploy}
+                className="hover:scale-125 transition-transform duration-200 ease-in-out"
+                >
                     <InlineIcon className='text-[1.9rem]' icon={livePreview} />
                 </a>
             </div>
