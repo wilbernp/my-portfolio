6a71471162548a24823de856648416d3b1399d0e
wilbernp <wilbernarvaezpetro@gmail.com>
2023-02-26 15:17:33 -0500

    add custom colors to buttons

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,14 @@
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     title: string;
-    secondary?:boolean;
+    secondary?: boolean;
+    color?: "alert" | "danger"
 }
 
-export default function Button({ title, secondary, type }: ButtonProps) {
-    const Primary = <button type={type} className="p-2 w-full border-4 rounded-3xl border-blue-400 text-blue-400">{title}</button>
-    
-    const Secondary = <button type={type} className="w-full p-2 border-4 rounded-3xl border-blue-400 bg-blue-400 text-[#072146] text-base font-medium">{title}</button>
+export default function Button({ title, secondary, type, color }: ButtonProps) {
+    const Primary = <button type={type} className={`p-2 w-full border-4 rounded-3xl border-${color || "secondary"} text-${color || "secondary"}`}>{title}</button>
+
+    const Secondary = <button type={type} className={`w-full p-2 border-4 rounded-3xl border-${color || "secondary"} bg-${color || "secondary"} text-[#072146] text-base font-medium`}>{title}</button>
 
     if (secondary) {
         return Secondary
