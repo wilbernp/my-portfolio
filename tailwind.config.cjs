abbb2454f34e42b8e6802e74e2187954d570aa7e
wilbernp <wilbernarvaezpetro@gmail.com>
2023-02-26 15:16:49 -0500

    add config colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -11,13 +12,22 @@ module.exports = {
       'xs': '475px',
       ...defaultTheme.screens,
     },
+    colors: {
+      "main": "#022873",
+      "primary": "#035AA6",
+      "secondary": "#05C7F2",
+      "alert": "#F2C063",
+      "danger": "#F25E7A",
+      "font-color": "#d5e1f1",
+      ...colors
+    },
     extend: {
       spacing: {
         container: "2.5rem",
-        nav:"5rem"
+        nav: "5rem"
       },
-      fontSize:{
-        "xs":["0.95rem",{lineHeight:"1rem"}]
+      fontSize: {
+        "xs": ["0.95rem", { lineHeight: "1rem" }]
       },
       backgroundImage: {
         'contact-image': "url('./src/assets/contactImage.svg')",
